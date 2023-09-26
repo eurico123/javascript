7289f39612abd8eccb5bcc712d4200b3cefd885a
Henrique Almeida Soares <hg515900@gmail.com>
2023-09-26 11:12:49 -0300

    criei exercícios de condição

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 54
+console.log('A velocidade do seu carro é de ' + 54 + 'km/h')
+if (vel > 60){ // condição simples!
+    console.log('Você utrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Diriga sempre usando cinto de segurança!')
